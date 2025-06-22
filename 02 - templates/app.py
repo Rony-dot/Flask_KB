d1baf99bc6405eae1206d027c62f043bcb8c42db
Rakibul <rakibulhassan.rony123@gmail.com>
2025-06-22 12:35:50 +0600

    jinja features, dynamic urls, redirect, url_for etc

--- 02 - templates/app.py
+++ 02 - templates/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__, template_folder='templates')
 
@@ -16,8 +16,16 @@ def home_page():
 @app.route("/jinja")
 def some_jinja():
     list_of_even_numbers = [2, 4, 6, 8, 10]
-    return render_template("basic_jinja.html", name="rakibulH", email="john@gmail.com", numbers=list_of_even_numbers)
+    return render_template("basic_jinja.html", name="rakibul hassan", email="john@gmail.com", numbers=list_of_even_numbers)
 
+
+@app.template_filter("repeat")
+def repeat_value(value, times=2):
+    return value*times
+
+@app.route("/redirect")
+def redirect_page():
+    return redirect(url_for('some_jinja'))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
